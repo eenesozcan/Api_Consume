d578e255d463988c832eff729080e991b7ef60e2
Enes OZCAN <enesozcan@hotmail.com>
2022-12-23 22:47:35 +0300

    23.12.2022

--- Api_Consume/Controllers/DefaultController.cs
+++ Api_Consume/Controllers/DefaultController.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -29,7 +30,41 @@ namespace Api_Consume.Controllers
             }
             return View();
         }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
 
 
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(CategoryViewModel p)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44307/api/Category", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44307/api/Category/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<CategoryViewModel>(jsonData);
+                return View(result);
+            }
+            return View();
+        }
     }
 }
