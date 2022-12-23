fba6b81986159d80ad0640dc2426e6622354c485
Enes OZCAN <enesozcan@hotmail.com>
2022-12-23 22:25:24 +0300

    Add project files.

---
+++ Api_Consume/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using Api_Consume.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Api_Consume.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44307/api/Category");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<CategoryViewModel>>(jsonData);
+                return View(result);
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ Api_Consume/Models/CategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Api_Consume.Models
+{
+    public class CategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
